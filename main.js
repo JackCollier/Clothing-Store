5eb0754657e053111e3da1a6cda1143bdf85c259
JacvaSipt <steamcow12@hotmail.com>
2023-04-28 15:13:46 +0100

    add basket function working with increment

--- main.js
+++ main.js
@@ -1,34 +1,40 @@
-let shop = document.getElementById('shop')
-let shopItemsData = [{
-    id:'S1',
-    name:'Tube Tee',
-    price:45,
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
-    img: 'images/Tubes mock.png'
-},{
-    id:'S2',
-    name:'Marcolina Tee ',
-    price:49,
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
-    img: 'images/Marcolina invert mock.png'
-},{
-    id:'S3',
-    name:'Window Tee',
-    price:40,
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
-    img: 'images/Window mockup.png'
-},{
-    id:'S4',
-    name:'Berlin Tee',
-    price:48,
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
-    img: 'images/BERLIN Mockup.png'
-}]
-
+let shop = document.getElementById("shop");
+let shopItemsData = [
+  {
+    id: "S1",
+    name: "Tube Tee",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/Tubes mock.png",
+  },
+  {
+    id: "S2",
+    name: "Marcolina Tee ",
+    price: 49,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/Marcolina invert mock.png",
+  },
+  {
+    id: "S3",
+    name: "Window Tee",
+    price: 40,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/Window mockup.png",
+  },
+  {
+    id: "S4",
+    name: "Berlin Tee",
+    price: 48,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/BERLIN Mockup.png",
+  },
+];
+let basket = [];
 let generateShop = () => {
-    return shop.innerHTML = shopItemsData.map(x => {
-        let {id,name,price,desc,img} = x
-        return `
+  return (shop.innerHTML = shopItemsData
+    .map((x) => {
+      let { id, name, price, desc, img } = x;
+      return `
         <figure id=product-id-${id} class="item">
                 <img width="220" src="${img}" alt="Store item one">
                 <figcaption class="details" >
@@ -44,21 +50,27 @@ let generateShop = () => {
                     </div>
                 </figcaption>
             </figure>
-        `
-    }).join("")
-}
+        `;
+    })
+    .join(""));
+};
 
-generateShop()
+generateShop();
 
-let increment = id => { /* id being passed refers to the property of the product clicked */
-    console.log(id)
-}
+/* Inside the increment function, you can use this id value to look up the quantity div by calling document.getElementById(id). The id value passed as an argument to the function is the same as the id attribute of the quantity div, so JavaScript can use this value to find the correct quantity div using document.getElementById(id).
+So you don't need to explicitly call document.getElementById(id) inside the increment function, because the id value is passed as an argument in a way that allows it to be used directly as an argument to document.getElementById(). */
 
-let decrement = id => {
-    console.log(id)
-}
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+  /* If basket doesn't cotain selected item, add to array. If it does, increase item by 1 */
+  search === undefined ? basket.push({id: selectedItem.id, item: 1 }) : search.item += 1 
+  console.log(basket);
+};
 
-let update = () => {
-    
-}
+let decrement = (id) => {
+  let selectedItem = id;
+  console.log(id);
+};
 
+let update = () => {};
