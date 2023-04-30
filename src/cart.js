a6104339150ea19adc74ec554e136762350731ed
JacvaSipt <steamcow12@hotmail.com>
2023-04-30 11:57:27 +0100

    add cart is empty element

--- src/cart.js
+++ src/cart.js
@@ -1,6 +1,27 @@
+let label = document.getElementById('label')
+let shoppingCart = document.getElementById('shopping-cart')
 let basket = JSON.parse(localStorage.getItem("data")) || [];
-let calculation = x => {
-    let cartAmount = document.getElementById('cartAmount')
-    cartAmount.innerText = basket.map(x => x.item).reduce((acc,val) => acc + val,  0)
+
+let calculation = (x) => {
+  let cartAmount = document.getElementById("cartAmount");
+  cartAmount.innerText = basket
+    .map((x) => x.item)
+    .reduce((acc, val) => acc + val, 0);
+};
+calculation();
+
+let generateCartItems = () => {
+    if(basket.length !== 0) {
+        
+    }
+    else {
+        shoppingCart.innerHTML = ``
+        label.innerHTML = `
+            <h2> Cart is Empty </h2>
+            <a href='index.html'>
+                <button class="homeBtn">Back to Home Page</button>
+            </a>
+        `
+    }
 }
-calculation()
+generateCartItems()
