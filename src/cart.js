3c6ed3e4d95c4a88a5a642a0f94004c36a8058e4
JacvaSipt <steamcow12@hotmail.com>
2023-04-30 12:29:47 +0100

    add basic structure for cart page items

--- src/cart.js
+++ src/cart.js
@@ -1,5 +1,5 @@
-let label = document.getElementById('label')
-let shoppingCart = document.getElementById('shopping-cart')
+let label = document.getElementById("label");
+let shoppingCart = document.getElementById("shopping-cart");
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let calculation = (x) => {
@@ -11,17 +11,37 @@ let calculation = (x) => {
 calculation();
 
 let generateCartItems = () => {
-    if(basket.length !== 0) {
-        
-    }
-    else {
-        shoppingCart.innerHTML = ``
-        label.innerHTML = `
+  if (basket.length !== 0) {
+    return (shoppingCart.innerHTML = basket
+      .map((x) => {
+        let { id, item } = x;
+        let search = shopItemsData.find((y) => y.id === id) || [];
+        return `
+            <div class="cart-item">
+               <img width="100" src="${search.img}" alt="">
+               <div class="details">
+                 <div class="title-price-x">
+                    <h4>
+                       <p>${search.name}</p>
+                       <p>$ ${search.price}</p>
+                    </h4>
+                    <i class="bi bi-x-lg"></i>
+                 </div>
+                 <div class="cart-buttons"></div>
+                 <h3></h3>
+               </div>
+            </div>
+            `;
+      })
+      .join(""));
+  } else {
+    shoppingCart.innerHTML = ``;
+    label.innerHTML = `
             <h2> Cart is Empty </h2>
             <a href='index.html'>
                 <button class="homeBtn">Back to Home Page</button>
             </a>
-        `
-    }
-}
-generateCartItems()
+        `;
+  }
+};
+generateCartItems();
