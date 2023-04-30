78221ce54b7d38cb7ee4d23cfe898513a9dab351
JacvaSipt <steamcow12@hotmail.com>
2023-04-30 11:31:10 +0100

    move data to new file and link

--- src/main.js
+++ src/main.js
@@ -1,34 +1,4 @@
 let shop = document.getElementById("shop");
-let shopItemsData = [
-  {
-    id: "S1",
-    name: "Tube Tee",
-    price: 45,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/Tubes mock.png",
-  },
-  {
-    id: "S2",
-    name: "Marcolina Tee ",
-    price: 49,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/Marcolina invert mock.png",
-  },
-  {
-    id: "S3",
-    name: "Window Tee",
-    price: 40,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/Window mockup.png",
-  },
-  {
-    id: "S4",
-    name: "Berlin Tee",
-    price: 48,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: "images/BERLIN Mockup.png",
-  },
-];
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 let generateShop = () => {
   return (shop.innerHTML = shopItemsData
